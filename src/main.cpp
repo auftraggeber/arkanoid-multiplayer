0076878491d6fe0cab4186a669928e313a9c002c
Jonas Langner <jonas.langner.kuz@fh-zwickau.de>
2023-05-23 13:27:43 +0000

    Basisprozeduren zum Verbindungsaufbau.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,16 @@
 
 #include <fmt/format.h>
 #include <string>
+#include <thread>
 #include <vector>
 
-#include "asio.hpp"
+#include "boost/asio.hpp"
 
+#include "boost/asio/io_service.hpp"
+#include "boost/asio/ip/address.hpp"
+#include "boost/asio/ip/tcp.hpp"
+#include "boost/asio/ip/udp.hpp"
+#include "boost/system/system_error.hpp"
 #include "fmt/core.h"
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/event.hpp"
@@ -19,14 +25,41 @@
 namespace connection {
 
 int constexpr default_port{ 45678 };
+std::string const default_host{ "localhost" };
 
+[[nodiscard]] boost::asio::ip::tcp::socket connect_to(std::string const &host, int const &port)
+{
+  using namespace boost::asio::ip;
 
+  boost::asio::io_service io_service;
+
+  tcp::socket socket{ io_service };
+
+  socket.connect(tcp::endpoint(address::from_string(host), port));
+
+  return socket;
 }
+
+[[nodiscard]] boost::asio::ip::tcp::socket wait_for_connection(int const &port)
+{
+  using namespace boost::asio::ip;
+
+  boost::asio::io_service io_service;
+
+  tcp::acceptor acceptor{ io_service, tcp::endpoint(tcp::v4(), port) };
+
+  tcp::socket socket{ io_service };
+  acceptor.accept(socket);
+
+  return socket;
+}
+
+}// namespace connection
 
 namespace menu {
 using namespace ftxui;
 
-void show_connecting()
+ScreenInteractive show_connecting()
 {
   auto screen = ScreenInteractive::TerminalOutput();
 
@@ -41,6 +74,8 @@ void show_connecting()
   });
 
   screen.Loop(renderer);
+
+  return screen;
 }
 
 [[nodiscard]] int calculate_port(std::string const &str)
@@ -52,29 +87,32 @@ void show_connecting()
   return connection::default_port;
 }
 
-void show_waiting_for_connection(int const &port = connection::default_port) {}
+ScreenInteractive show_waiting_for_connection(int const &port = connection::default_port)
+{
+  auto screen = ScreenInteractive::TerminalOutput();
 
-void show_connection_methods()
+  return screen;
+}
+
+void show_connection_methods(std::function<void(bool const &, int const &)> callback)
 {
 
   auto screen = ScreenInteractive::TerminalOutput();
-  bool is_host{ false };
 
+  bool is_host{ false };
   std::string port_string{ std::to_string(connection::default_port) };
+
 
   auto button_host = Button("Als Host auf Verbindung warten", [&] {
     screen.Exit();
     is_host = true;
   });
-
   auto button_client = Button("Zu einem Host verbinden", [&] {
     screen.Exit();
     is_host = false;
   });
-
   auto port_input = Input(&port_string, "Geben Sie den Port ein (Verbinden zu / Warten auf)");
 
-  // The tree of components. This defines how to navigate using the keyboard.
   auto children = Container::Vertical({ port_input, Container::Horizontal({ button_host, button_client }) });
 
 
@@ -90,19 +128,42 @@ void show_connection_methods()
 
   screen.Loop(render);
 
+
   int const port = calculate_port(port_string);
 
-  if (is_host) {
-    show_waiting_for_connection(1);
-  } else {
-    show_connecting();
-  }
+  callback(is_host, port);
 }
 
 }// namespace menu
 
+[[nodiscard]] boost::asio::ip::tcp::socket connect_to_peer(bool const as_host, int const port)
+{
+  if (as_host) { return connection::wait_for_connection(port); }
+
+  return connection::connect_to(connection::default_host, port);
+}
+
+[[nodiscard]] ftxui::ScreenInteractive show_connecting(bool const as_host, int const port)
+{
+  if (as_host) {
+    return menu::show_waiting_for_connection(port);
+  } else {
+    return menu::show_connecting();
+  }
+}
+
 int main()
 {
-  menu::show_connection_methods();
+  menu::show_connection_methods([](bool const &as_host, int const &port) {
+    auto screen = show_connecting(as_host, port);
+
+    try {
+      boost::asio::ip::tcp::socket peer = connect_to_peer(as_host, port);
+      screen.Exit();
+    } catch (boost::system::system_error e) {
+      fmt::print("Es konnte keine Verbindung hergestellt werden.");
+      screen.Exit();
+    }
+  });
   return 0;
 }
